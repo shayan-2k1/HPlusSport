f1e211855f381802c6ad9e1d0ec254ff144fbb1b
Muhammad Shayan Khan <shayan.956khan@gmail.com>
2024-08-15 14:38:57 +0500

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HPlusSport.API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Sku { get; set; } = String.Empty;
+        public string Name { get; set; } = String.Empty;
+
+        public string Description { get; set; } = String.Empty;
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [JsonIgnore]
+        public virtual Category? Category {get; set;}
+
+
+    }
+}
---
+++ Models/ProductQueryParameters.cs
@@ -0,0 +1,11 @@
+﻿namespace HPlusSport.API.Models
+{
+    public class ProductQueryParameters: QueryParameters
+    {
+        public decimal? MaxPrice { get; set; }
+        public decimal? MinPrice { get; set; }
+
+        public string Name { get; set; }=string.Empty;
+        public string Sku { get; set; }= string.Empty;
+    }
+}
